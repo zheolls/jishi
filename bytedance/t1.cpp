ea752c60dedf9a01c4bf6b0a33f1a8e38703ca01
zheolls <zhusaihui@gmail.com>
2020-03-15 20:35:14 +0800

    v3

--- bytedance/t1.cpp
+++ bytedance/t1.cpp
@@ -1,4 +1,66 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include<vector>
 using namespace std;
+struct t1
+{
+    string src;
+    vector<string>  dst=vector<string>();
+};
+vector<t1> ti=vector<t1>();
+int getindex(string str){
+    for (int a=0;a<(int)ti.size();a++){
+        if (ti[a].src==str){
+            return a;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    string str1,str2;
+    while (n--)
+    {
+        cin>>str1>>str2;
+        int a=getindex(str2);
+        int flag=1;
+        if (a>=0){
+            for (auto i = ti[a].dst.begin(); i < ti[a].dst.end() ; i++)
+            {
+                if ((*i)==str1){
+                    flag=0;
+                    break; 
+                }
+                if (flag){
+                    ti[a].dst.push_back(str1);
+                }
+            }        
+        }
+        else
+        {
+            t1 temp;
+            temp.src=str2;
+            temp.dst.push_back(str1);
+            ti.push_back(temp);
+        }
+        
+    }
+    for (auto i = ti.begin(); i < ti.end(); i++)
+    {
+        cout<<(*i).src;
+        for (auto j = (*i).dst.begin(); j < (*i).dst.end(); j++)
+        {
+            cout<<" "<<(*j);
+        }
+        cout<<endl;
+        
+    }
+    
+
+    
+}
--- bytedance/t2.cpp
+++ bytedance/t2.cpp
@@ -2,3 +2,84 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+int position[2000];
+int DELTA=0;
+int max(int a,int b){
+    int maxn=0,j=a;;
+    for (auto i = a+1; i<DELTA && position[a]+b > position[i]  ; i++)
+    {
+        if (maxn<position[DELTA+i]){
+            maxn=position[DELTA+i];
+            j=i;
+        }
+    }
+    return j;
+    
+}
+int main(){
+    int D,W;
+    cin>>D>>W;
+    int i=0;
+    int cishu=0;
+    while (cin>>position[DELTA])
+    {
+        DELTA++;
+    }
+    DELTA=DELTA/2;
+    int a=0,b=W,d;
+    int maxn=0;
+    if (position[0]>W)
+    {
+        cout<<-1<<endl;
+        return 0;
+    }
+    else if (W>=D)
+    {
+        cout<<0<<endl;
+        return 0;
+    }
+    else
+    {
+        for (auto i = 0; W > position[i]  ; i++)
+        {
+            if (maxn<position[DELTA+i]){
+                maxn=position[DELTA+i];
+                a=i;
+            }
+        }
+        W=W+position[DELTA+a]-position[a];
+        cishu++;
+        // cout<<a<<endl;
+    }
+    d=a;
+    while (true)
+    {
+        if (position[d]+W>=D)
+        {
+            cout<<cishu<<endl;
+            break;
+        }
+        
+        d=max(a,W);
+        // cout<<d<<endl;
+        if (d==a)
+        {
+            cout<<-1<<endl;
+            break;
+        }
+        
+        // if (position[DELTA+d]+W>=D)
+        // {
+        //     cout<<cishu<<endl;
+        //     break;
+        // }
+        W = W - (position[d]-position[a])+position[DELTA+d];
+        a=d;
+        cishu++;
+
+    }
+    
+    
+    
+
+}
